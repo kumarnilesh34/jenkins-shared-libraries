a71d29b17c0d0697f1d0d23ec09f9c85aa7ab7ab
kumarnilesh34 <kumar.nilesh34@gmail.com>
2025-06-18 14:33:00 +0530

    Create dockerContainerDeploy.groovy

---
+++ vars/dockerContainerDeploy.groovy
@@ -0,0 +1,6 @@
+def call(String containerName, String dockerHubUser, String imageName, String imageTag, String portMapping, String containerName){
+}
+    sh "docker stop ${containerName} || true"
+    sh "docker rm ${containerName} || true"
+    sh "docker run -itd --name ${containerName} -p ${portMapping} $dockerHubUser/${imageName}:$imageTag"
+}
